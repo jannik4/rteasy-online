59d63d39441f18c7ecb73e4e985ad10971e1746c
Jannik Obermann <jannik.obermann@gmail.com>
2021-08-23 15:04:47 +0200

    Implement Clone for ast types

--- src/rt-easy-core/src/ast.rs
+++ src/rt-easy-core/src/ast.rs
@@ -3,14 +3,14 @@ use std::ops::Range;
 pub use crate::common::*;
 pub use either::Either;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Ast<'s> {
     pub declarations: Vec<Declaration<'s>>,
     pub statements: Vec<Statement<'s>>,
     pub trailing_label: Option<Label<'s>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Declaration<'s> {
     Register(DeclareRegister<'s>),
     Bus(DeclareBus<'s>),
@@ -18,65 +18,65 @@ pub enum Declaration<'s> {
     RegisterArray(DeclareRegisterArray<'s>),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct DeclareRegister<'s> {
     pub registers: Vec<RegBus<'s>>,
     pub span: Range<usize>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct DeclareBus<'s> {
     pub buses: Vec<RegBus<'s>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct DeclareMemory<'s> {
     pub memories: Vec<Memory<'s>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct DeclareRegisterArray<'s> {
     pub register_arrays: Vec<DeclareRegisterArrayItem<'s>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct RegBus<'s> {
     pub ident: Ident<'s>,
     pub range: Option<BitRange>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Concat<'s> {
     pub parts: Vec<ConcatPart<'s>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum ConcatPart<'s> {
     RegBus(RegBus<'s>),
     RegisterArray(RegisterArray<'s>),
     Number(Number),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Memory<'s> {
     pub ident: Ident<'s>,
     pub range: MemoryRange<'s>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct DeclareRegisterArrayItem<'s> {
     pub ident: Ident<'s>,
     pub range: Option<BitRange>,
     pub len: usize,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct RegisterArray<'s> {
     pub ident: Ident<'s>,
     pub index: Box<Expression<'s>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Statement<'s> {
     pub label: Option<Label<'s>>,
     pub operations: Vec<Operation<'s>>,
@@ -84,61 +84,79 @@ pub struct Statement<'s> {
     pub span: Range<usize>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Operation<'s> {
     pub kind: OperationKind<'s>,
     pub span: Range<usize>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum OperationKind<'s> {
     Nop(Nop),
     Goto(Goto<'s>),
     Write(Write<'s>),
     Read(Read<'s>),
     If(If<'s>),
+    Switch(Switch<'s>),
     Assignment(Assignment<'s>),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Nop;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Goto<'s> {
     pub label: Label<'s>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Write<'s> {
     pub ident: Ident<'s>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Read<'s> {
     pub ident: Ident<'s>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct If<'s> {
     pub condition: Expression<'s>,
     pub operations_if: Vec<Operation<'s>>,
     pub operations_else: Option<Vec<Operation<'s>>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
+pub struct Switch<'s> {
+    pub expression: Expression<'s>,
+    pub clauses: Vec<Either<CaseClause<'s>, DefaultClause<'s>>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct CaseClause<'s> {
+    pub value: Expression<'s>,
+    pub operations: Vec<Operation<'s>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct DefaultClause<'s> {
+    pub operations: Vec<Operation<'s>>,
+}
+
+#[derive(Debug, Clone)]
 pub enum Lvalue<'s> {
     RegBus(RegBus<'s>),
     RegisterArray(RegisterArray<'s>),
     Concat(Concat<'s>),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Assignment<'s> {
     pub lhs: Lvalue<'s>,
     pub rhs: Expression<'s>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Expression<'s> {
     Atom(Atom<'s>),
     BinaryTerm(Box<BinaryTerm<'s>>),
@@ -163,7 +181,7 @@ impl<'s> From<UnaryTerm<'s>> for Expression<'s> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Atom<'s> {
     Concat(Concat<'s>),
     RegBus(RegBus<'s>),
@@ -171,14 +189,14 @@ pub enum Atom<'s> {
     Number(Number),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct BinaryTerm<'s> {
     pub lhs: Expression<'s>,
     pub rhs: Expression<'s>,
     pub operator: BinaryOperator,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct UnaryTerm<'s> {
     pub expression: Expression<'s>,
     pub operator: UnaryOperator,
