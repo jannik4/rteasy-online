7ae85bda92d930c9b6732495c0ff86e27cdbf29d
jannik4 <50554200+jannik4@users.noreply.github.com>
2022-03-07 15:58:22 +0000

    Deploying to gh-pages from @ jannik4/rteasy-online@cc01ec0fb7756241e19355ecfedd3fecb3dc850f 🚀

--- docs/backend/implementors/alloc/borrow/trait.ToOwned.js
+++ docs/backend/implementors/alloc/borrow/trait.ToOwned.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["value"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.1/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"value/struct.ValueSlice.html\" title=\"struct value::ValueSlice\">ValueSlice</a>","synthetic":false,"types":["value::slice::ValueSlice"]}];
+implementors["value"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.59.0/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"value/struct.ValueSlice.html\" title=\"struct value::ValueSlice\">ValueSlice</a>","synthetic":false,"types":["value::slice::ValueSlice"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- docs/backend/implementors/core/marker/trait.Sized.js
+++ docs/backend/implementors/core/marker/trait.Sized.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["value"] = [{"text":"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.1/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> for <a class=\"struct\" href=\"value/struct.ValueSlice.html\" title=\"struct value::ValueSlice\">ValueSlice</a>","synthetic":true,"types":["value::slice::ValueSlice"]}];
+implementors["value"] = [{"text":"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.59.0/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> for <a class=\"struct\" href=\"value/struct.ValueSlice.html\" title=\"struct value::ValueSlice\">ValueSlice</a>","synthetic":true,"types":["value::slice::ValueSlice"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
